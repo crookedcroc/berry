834995e40dedec00aa2a37abe03b24f94ceaa443
crookedcroc <135115656+crookedcroc@users.noreply.github.com>
2023-06-01 11:46:05 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end();
+}).listen(3000);
+
+exports.myDateTime = function () {
+  return Date();
+};
