80bb6d002aee4eb01b7299f5e65e9100d06862bb
crookedcroc <135115656+crookedcroc@users.noreply.github.com>
2023-06-01 11:47:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end();
-}).listen(3000);
+}).listen(8080);
 
 exports.myDateTime = function () {
   return Date();
